999b11a05f18f12e46635d72758d1ff70d2b156a
Carles <carles.sanchez@namshi.com>
2019-03-21 08:29:54 +0400

    Added fp map and filter

--- collections/index.js
+++ collections/index.js
@@ -1,2 +1,7 @@
 Object.prototype.map = function(fn){ return Object.entries(this).reduce((acc,[k,v]) => ({...acc,[k]:fn(v,k)}),{}); };
 Object.prototype.filter = function(fn){ return Object.entries(this).reduce((acc,[k,v]) => fn(v,k) && ({...acc,[k]:v}),{}) || acc; };
+
+const map = fn => col => col.map(fn);
+const filter = fn => col => col.filter(fn);
+
+module.exports = {map, filter};
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = { ...require('./fp'), ...require('./object') };
+module.exports = { ...require('./fp'), ...require('./collections') };
