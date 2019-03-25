2d790557234904d5e1268eedf2324dcaa6df10eb
Carles <carles.sanchez@namshi.com>
2019-03-25 08:45:54 +0400

    Added best concurrency pattern

--- concurrency/index.js
+++ concurrency/index.js
@@ -1,1 +1,11 @@
 Promise.any = (promises) => Promise.all(promises.map(promise => promise.then(value => ({value})).catch(error => ({error}))));
+
+Promise.best = (promises) => new Promise(function(resolve,reject) {
+    let errors = [];
+    promises.map(promise => promise.then(data => resolve(data)).catch(err => {
+        errors.push(err);
+        if(errors.length === promises.length){
+            reject(errors);
+        }
+    }));
+})
