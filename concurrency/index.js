a0350cc9e423b123369b156b0f6e67bd06f20768
Carles <carles.sanchez@namshi.com>
2019-03-25 09:15:16 +0400

    Added prettier, editorconfig.

--- concurrency/index.js
+++ concurrency/index.js
@@ -1,11 +1,35 @@
-Promise.any = (promises) => Promise.all(promises.map(promise => promise.then(value => ({value})).catch(error => ({error}))));
+Promise.any = promises => Promise.all(promises.map(promise => promise.then(value => ({ value })).catch(error => ({ error }))));
 
-Promise.best = (promises) => new Promise(function(resolve,reject) {
+Promise.best = promises =>
+  new Promise(function(resolve, reject) {
     let errors = [];
-    promises.map(promise => promise.then(data => resolve(data)).catch(err => {
-        errors.push(err);
-        if(errors.length === promises.length){
+    promises.map(promise =>
+      promise
+        .then(data => resolve(data))
+        .catch(err => {
+          errors.push(err);
+          if (errors.length === promises.length) {
             reject(errors);
-        }
-    }));
-})
+          }
+        })
+    );
+  });
+
+const circuitBreak = ({ tries = 3, cooldown = 1000, _tries = 1, _time = 0 } = {}) => (fn, fail, breaker) => (...args) => {
+  if (tries >= _tries) {
+    try {
+      return fn.apply(null, args);
+    } catch (e) {
+      _tries++;
+      _time = new Date();
+      return fail(e);
+    }
+  } else {
+    if (new Date() - _time > cooldown) {
+      _tries = 1;
+    }
+    breaker && breaker.apply(null, args);
+  }
+};
+
+module.exports = { circuitBreak };
