7896859c0b3903f9e975df2cf3dc4e1a07fbb08d
Carles <carles.sanchez@namshi.com>
2019-03-26 17:04:30 +0400

    Testing either

--- concurrency/index.js
+++ concurrency/index.js
@@ -1,34 +1,31 @@
+const { either } = require('../logic');
+
 Promise.any = promises => Promise.all(promises.map(promise => promise.then(value => ({ value })).catch(error => ({ error }))));
 
 Promise.best = promises =>
-  new Promise(function(resolve, reject) {
-    let errors = [];
+  new Promise(function(resolve, reject, errors = []) {
     promises.map(promise =>
-      promise
-        .then(data => resolve(data))
-        .catch(err => {
-          errors.push(err);
-          if (errors.length === promises.length) {
-            reject(errors);
-          }
-        })
+      promise.then(resolve).catch(err => {
+        errors.push(err);
+        if (errors.length === promises.length) {
+          reject(errors);
+        }
+      })
     );
   });
 
 const circuitBreak = ({ tries = 3, cooldown = 1000, _tries = 1, _time = 0 } = {}) => (fn, fail, breaker) => (...args) => {
   if (tries >= _tries) {
-    try {
-      return fn.apply(null, args);
-    } catch (e) {
+    return either(fn, err => {
       _tries++;
       _time = new Date();
-      return fail(e);
-    }
+      return fail(err);
+    })(...args);
   } else {
     if (new Date() - _time > cooldown) {
       _tries = 1;
     }
-    breaker && breaker.apply(null, args);
+    breaker && breaker(...args);
   }
 };
 
