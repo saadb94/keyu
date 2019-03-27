398de3e7ed372e7f6464f34964375e935ae348d5
Carles <kirdie@gmail.com>
2019-03-27 20:05:20 +0400

    Added tests, added travis-ci, fixed bug

--- collections/index.js
+++ collections/index.js
@@ -1,5 +1,5 @@
 Object.prototype.map = function(fn){ return Object.entries(this).reduce((acc,[k,v]) => ({...acc,[k]:fn(v,k)}),{}); };
-Object.prototype.filter = function(fn){ return Object.entries(this).reduce((acc,[k,v]) => fn(v,k) && ({...acc,[k]:v}),{}) || acc; };
+Object.prototype.filter = function(fn){ return Object.entries(this).reduce((acc,[k,v]) => fn(v,k) && ({...acc,[k]:v}),{}) || {}; };
 
 const map = fn => col => col.map(fn);
 const filter = fn => col => col.filter(fn);
